ac37b53b0551dd4cc8ad9892e39d3680cb0301a1
Martin Sucha <martin.sucha@fmph.uniba.sk>
2014-02-04 17:44:53 +0100

    Pridana samostatna praca do zoznamu cinnosti

--- import.py
+++ import.py
@@ -37,7 +37,7 @@ def process_file(filename, lang='sk'):
     map_metodyStudia = {u'prezenčná': 'P', u'dištančná': 'D', u'kombinovaná': 'K'}
     map_druhCinnosti = {u'Kurz': 'K', u'Prednáška': 'P', u'Cvičenie': 'C',
             u'Seminár': 'S', u'Laboratórne cvičenie': 'L', 'Prax': 'A', u'Iná':
-            'N'}
+            'N', u'Samostatná práca': 'B'}
 
     data = []
 
